ac78989a1e9a3398bca1a904e8e1ffe461f9ee20
Mark Blanford <markytorture@hotmail.com>
2017-12-12 17:46:48 +0000

    controller tested and working

--- controller.rb
+++ controller.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
-require_relative('/models/rock_paper_scissors.rb')
+require_relative('models/rock_paper_scissors.rb')
 
 get('/game/:hand1/:hand2') do
   game = RockPaperScissors.new(params[:hand1], params[:hand2])
