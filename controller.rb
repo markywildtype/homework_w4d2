7d887ac8eca5f647b783ac7e40ddebb84bbc7e0e
Mark Blanford <markytorture@hotmail.com>
2017-12-12 18:01:54 +0000

    added welcome page and game page

--- controller.rb
+++ controller.rb
@@ -3,6 +3,14 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('models/rock_paper_scissors.rb')
 
+get('/') do
+  erb(:welcome)
+end
+
+get('/game') do
+  erb(:game)
+end
+
 get('/game/:hand1/:hand2') do
   game = RockPaperScissors.new(params[:hand1], params[:hand2])
   @game_result = game.play()
